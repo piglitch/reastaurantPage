16666fcfb7abc159efcbbef02973c918aa504b6d
Avi <avibanerjee2000@gmail.com>
2023-10-24 22:36:39 +0530

    added babel

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,19 @@ module.exports = {
     module:{
         rules: [
             {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: [
+                      ['@babel/preset-env', { targets: "defaults" }]
+                    ],
+                    plugins: ['@babel/plugin-proposal-class-properties']
+                  }
+                }
+              },
+            {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'], 
             },
